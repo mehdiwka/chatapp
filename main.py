f4ab3e7d4e279b8eecfd0ba4af8339ea88375f00
mehdi <mehdi.hosseinvand@gmail.com>
2023-10-14 11:00:31 +0330

    first commit.

---
+++ main.py
@@ -0,0 +1,57 @@
+from typing import List
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Request, Response
+from fastapi.templating import Jinja2Templates
+
+from schemas import User
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def broadcast(self, data: str):
+        for connection in self.active_connections:
+            await connection.send_text(data)
+
+
+manager = ConnectionManager()
+
+
+
+
+@app.get("/")
+def root(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
+
+
+@app.get("/chat")
+def get_chat(request: Request):
+    return templates.TemplateResponse("chat.html", {"request": request})
+
+
+@app.post("/login")
+def login(user: User, response: Response):
+    response.set_cookie(key="username", value=user.username)
+    return {"ok": True}
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.broadcast(data)
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast("Client left the chat.")

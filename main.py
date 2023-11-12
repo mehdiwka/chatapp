30430d86115a24a99a0b8ea9c8d383661200ebee
mehdi <mehdi.hosseinvand@gmail.com>
2023-11-12 18:13:18 +0330

    create a sample route for creating a user.

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ from typing import List
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Request, Response
 from database import engine, get_db_session, Base
 from fastapi.concurrency import run_in_threadpool
+from sqlalchemy.exc import SQLAlchemyError
 
-from schemas import User
+from fastapi import FastAPI, HTTPException, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from sqlalchemy.exc import IntegrityError
+from models import User
+from schemas import UserIn
+from database import get_db_session
 
 app = FastAPI()
 
@@ -17,6 +24,20 @@ async def startup():
 @app.on_event("shutdown")
 async def shutdown():
     await engine.disconnect()
+
+
+@app.post("/users/")
+async def create_user(user: UserIn, db: AsyncSession = Depends(get_db_session)):
+    async with db as db_session:
+        try:
+            db_user = User(number=user.number, username=user.username, name=user.name)
+            db_session.add(db_user)
+            await db_session.commit()
+            await db_session.refresh(db_user)
+            return db_user.__dict__
+        except SQLAlchemyError:
+            await db_session.rollback()
+            raise HTTPException(status_code=400, detail="Error while creating user.")
 
 # class ConnectionManager:
 #     def __init__(self):
--- schemas.py
+++ schemas.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel
 
 
-class User(BaseModel):
+class UserIn(BaseModel):
+    number: str
     username: str
+    name: str
